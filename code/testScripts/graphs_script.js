25d9738db7152ce9ce1afe7a1f20259bf60a4a77
jurgenvdb <35218025+jurgenvdb@users.noreply.github.com>
2024-02-21 16:57:55 +0100

    idk

--- code/testScripts/graphs_script.js
+++ code/testScripts/graphs_script.js
@@ -9,22 +9,13 @@ var startPoint = new vector(0, 0);
 var endPoint = new vector(1, 1);
 var steps = 100;
 var step = 1 / steps;
-var lineWidth = 1;
-var lineColor = "black";
-var absolutePointColor = "black";
-var pointColor = "red";
-var pointRadius = 5;
-var canvas = document.getElementById("brightness_graph");
-var ctx = canvas.getContext("2d");
-var width = canvas.width;
-var height = canvas.height;
 
 var controlPoints = [
     new vector(.5, .2), 
     new vector(.8, .5), 
 ];
 
-var graphValues = [["Time", "Brightness"]];
+var graphValues = [];
 generateData();
 
 function bezier(t) {
@@ -41,39 +32,26 @@ function bezier(t) {
 function generateData() {
   for (var t = 0; t <= 1; t += step) {
     var bez = bezier(t);
-    graphValues.push([bez.x, bez.y]);
+    graphValues.push({x: bez.x, y: bez.y});
   }
   console.log(graphValues);
 }
 
-function drawGraph() {
-  ctx.clearRect(0, 0, width, height);
-  ctx.beginPath();
-  ctx.moveTo(startPoint.x * width, startPoint.y * height);
-  for (var i = 1; i < graphValues.length; i++) {
-    ctx.lineTo(graphValues[i][0] * width, graphValues[i][1] * height);
-  }
-  ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = lineColor;
-  ctx.stroke();
-  ctx.closePath();
-  for (var i = 0; i < controlPoints.length; i++) {
-    ctx.beginPath();
-    ctx.arc(controlPoints[i].x * width, controlPoints[i].y * height, pointRadius, 0, 2 * Math.PI);
-    ctx.fillStyle = pointColor;
-    ctx.fill();
-    ctx.closePath();
-  }
-  ctx.beginPath();
-  ctx.arc(startPoint.x * width, startPoint.y * height, pointRadius, 0, 2 * Math.PI);
-  ctx.fillStyle = absolutePointColor;
-  ctx.fill();
-  ctx.closePath();
-  ctx.beginPath();
-  ctx.arc(endPoint.x * width, endPoint.y * height, pointRadius, 0, 2 * Math.PI);
-  ctx.fillStyle = absolutePointColor;
-  ctx.fill();
-  ctx.closePath();
-}
+var chart = new CanvasJS.Chart("brightness_graph",{
+  title :{
+    text: "Live Data"
+  },
+  axisX: {						
+    title: "Axis X Title"
+  },
+  axisY: {						
+    title: "Units"
+  },
+  data: [{
+    type: "line",
+    dataPoints : graphValues
+  }]
+});
 
-drawGraph();
+
+// https://canvasjs.com/docs/charts/how-to/creating-dynamic-charts/
--- code/testScripts/navigation_script.js
+++ code/testScripts/navigation_script.js
@@ -1,24 +1,24 @@
 var main_controls = document.getElementById("main_controls");
-        var alarm_clock = document.getElementById("alarm_clock");
-        var settings = document.getElementById("settings");
+var alarm_clock = document.getElementById("alarm_clock");
+var settings = document.getElementById("settings");
 
-        function switchVisible(element) {
-            // console.log("switchVisible")
-            // console.log(element.href.split("#")[1]);
-            if (element.href.split("#")[1] == main_controls.id) {
-                // console.log("main_controls");
-                main_controls.style.display = "block";
-                alarm_clock.style.display = "none";
-                settings.style.display = "none";
-            } else if (element.href.split("#")[1] == alarm_clock.id) {
-                // console.log("alarm_clock");
-                main_controls.style.display = "none";
-                alarm_clock.style.display = "block";
-                settings.style.display = "none";
-            } else if (element.href.split("#")[1] == settings.id) {
-                // console.log("settings");
-                main_controls.style.display = "none";
-                alarm_clock.style.display = "none";
-                settings.style.display = "block";
-            }
-        }
+function switchVisible(element) {
+    // console.log("switchVisible")
+    // console.log(element.href.split("#")[1]);
+    if (element.href.split("#")[1] == main_controls.id) {
+        // console.log("main_controls");
+        main_controls.style.display = "block";
+        alarm_clock.style.display = "none";
+        settings.style.display = "none";
+    } else if (element.href.split("#")[1] == alarm_clock.id) {
+        // console.log("alarm_clock");
+        main_controls.style.display = "none";
+        alarm_clock.style.display = "block";
+        settings.style.display = "none";
+    } else if (element.href.split("#")[1] == settings.id) {
+        // console.log("settings");
+        main_controls.style.display = "none";
+        alarm_clock.style.display = "none";
+        settings.style.display = "block";
+    }
+}
